1013a9bca2528617361002a63c787bab3842da10
RishithGandham <rishith.gandham@gmail.com>
2022-04-21 20:41:42 -0400

    fixed links

--- src/pages/funactivities.page.js
+++ src/pages/funactivities.page.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function FunActivitiesPage() {
 
-    
+
 
 
 
@@ -12,28 +12,20 @@ function FunActivitiesPage() {
             <div className='container'>
                 <h1 className='text-center display-3 mb-5'>Fun Activities</h1>
 
-
                 <h3 className='text-center'>Kahoot Links</h3>
                 <ol className='mb-5 '>
-                    <li><a href='https://kahoot.it/'>Diwali Kahoot</a></li>
-                    <li><a href='https://kahoot.it/'>Hannukah Kahoot</a></li>
+                    <li><a target="_blank" href='https://create.kahoot.it/share/diwali/1fba21cf-295b-46ff-b1c6-fb7d0a3828ab'>Diwali Kahoot</a></li>
+                    <li><a target="_blank" href='https://create.kahoot.it/share/hanukkah/d80b453d-bfe7-4cd0-8582-a5cb52c50938'>Hannukah Kahoot</a></li>
                 </ol>
 
                 <h3 className='text-center mt-5'>Youtube Video Guide Embeds</h3>
 
                 {/* bootstrap video */}
+                <h4></h4>
                 <div className='embed-responsive embed-responsive-16by9 mb-3'>
-                    <iframe className='embed-responsive-item' src='https://www.youtube.com/embed/'></iframe>
+                    <iframe className='embed-responsive-item' src="https://www.youtube.com/embed/Ik8ntEDhbfg" ></iframe>
                 </div>
-                <div className='embed-responsive embed-responsive-16by9 mb-3'>
-                    <iframe className='embed-responsive-item' src='https://www.youtube.com/embed/'></iframe>
-                </div>
-
-
-
-                <div className=''></div>
                 
-
             </div>
         </>
     )
